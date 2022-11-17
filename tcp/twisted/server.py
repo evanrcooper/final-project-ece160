3342fbac07b6fd77a6259f9fd1bf216d4313ff3c
James Ryan <james.paul9889@gmail.com>
2022-11-16 20:47:09 -0500

    gaming hours

--- tcp/twisted/server.py
+++ tcp/twisted/server.py
@@ -4,28 +4,26 @@ from twisted.internet import reactor
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 
-#FIX THE BOOKKEEPING
-maxConnections = 4 #max amount of clients we allow
-currentConnections = 0 #amount of clients connected
 
-
-class Uno(LineReceiver):
-    def __init__(self, players, addr):
+##TODO
+#add comments to describe whats happening
+#unify this to be JUST player logic.
+class UnoPlayers(LineReceiver):
+    def __init__(self, players, addr, maxCons):
         self.players = players #list of players
         self.addr = addr
         self.name = None
         self.state = "GETNAME"
+        self.maxConnections = maxCons
 
     def connectionMade(self):
         print(f"Player joined the lobby. Info: {self.addr}")
-        #currentConnections += 1
-        print(f"Num of current clients: {currentConnections}")
+        print(f"Num of current clients: NOT IMPLEMENTED")
         print(f"Waiting for more players....", end='\n\n')
         self.sendLine(b"Thanks for joining the game. Please choose an alias: ")
 
     def connectionLost(self, reason):
         print(f"Player {self.name} left the lobby. Info: {self.addr}")
-        #currentConnections -= 1
         if self.name.encode() in self.players:
             del self.players[self.name.encode()] 
         print(self.players)
@@ -44,9 +42,19 @@ class Uno(LineReceiver):
         self.players[name] = self
         self.state = "AWAITING_START"
         self.sendLine(b"Thanks. Waiting for everyone to be ready....")
-        self.sendLine(b"Current Lobby goes here.")
         self.sendLine(b"Options: r to change alias, q to quit.")
         print(f"{self.name} is ready!")
+        #iter through players here
+        #game start condition:
+        #all players are in state AWAITING_START and max players are achieved.
+        
+        if len(self.players) == self.maxConnections:
+            for name, protocol in self.players.items():
+                protocol.state = "BEGIN"
+                
+        for name, protocol in self.players.items(): #Players is a list of TUPLES. name in bytes, protocol is where the meat dwells.
+            print(name.decode(), end = ' ')
+            print(protocol.state)
         return
 
     def handle_AWAITING_START(self, line):
@@ -60,13 +68,25 @@ class Uno(LineReceiver):
         #print whose in the lobby
         #continually update
 
+class doNothing(UnoPlayers):
+    pass
+
 class unoFactory(Factory):
-    def __init__(self):
+    def __init__(self, maxCons):
         # sets up array for every player to be stuck into
         self.players = {}
-    
+        self.maxConnections = maxCons
+        self.currentCons = 0
+
     def buildProtocol(self, addr):
-        return Uno(self.players, addr)
+        if self.currentCons < self.maxConnections:
+            self.currentCons += 1
+            return UnoPlayers(self.players, addr, self.maxConnections)
+        else:
+            protocol = doNothing()
+            protocol.factory = self
+            return protocol
+            
 #https://stackoverflow.com/questions/30196186/how-to-limit-the-number-of-simultaneous-connections-in-twisted
 
 
@@ -75,7 +95,9 @@ class unoFactory(Factory):
 host = 'localhost'
 port = 8123
 
-reactor.listenTCP(port, unoFactory())
+maxConnections = 4
+
+reactor.listenTCP(port, unoFactory(maxConnections))
 print(f"Server started on host {host} port {port}")
 print(f"Today we will allow {maxConnections} people to play UNO. Waiting for all to join...")
 reactor.run()
