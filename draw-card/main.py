7556e835a0c01b7cb033a800150fca9923fe3e25
Evan Rosenfeld <evanr.college.22@gmail.com>
2022-12-01 16:18:56 -0500

    lol bozo

--- draw-card/main.py
+++ draw-card/main.py
@@ -9,26 +9,20 @@ import tkinter as tk
 from tkinter import *
 import math
 
-valid_cards = ('0','1','2','3','4','5','6','7','8','9','+','P','C','R','S','B')
+def draw(number, color = None):
+	if number in "0123456789":
+		drawCard(color, number)
+	elif number == '+':
+		drawCard(color, "+2")
+	elif number.upper() == 'P':
+		plus4()
+	elif number.upper() == 'C':
+		wild()
+	elif number.upper() == 'R':
+		reverse(color)
+	elif number.upper() == 'S':
+		skip(color)
+	elif number.upper() == 'B':
+		back()
 
-
-
-number = None
-while not number in valid_cards:
-	number = input("number: ").upper()
-
-if number in str({*range(10)}) or number == '+':
-	color = input("color: ")
-	drawCard(color, number)
-elif number == 'P':
-	plus4()
-elif number == 'C':
-	wild()
-elif number == 'R':
-	color = input("color: ")
-	reverse(color)
-elif number == 'S':
-	color = input("color: ")
-	skip(color)
-elif number == 'B':
-	back()
+draw(input("Number: "), input("Color: "))
