f986f6b51d57aa2a7d65fb6deb004aecc1c667cd
James Ryan <james.paul9889@gmail.com>
2022-12-01 21:19:05 -0500

    added comments and attributions
    	modified:   draw-card/main.py

--- draw-card/main.py
+++ draw-card/main.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python3
+
+"""
+##########################
+consolidation script to manage
+launching a new UNO card all
+under one file
+
+written by evan rosenfeld
+special thanks:
+    Prof. Robert Smyth for his
+    incredible tkinter know-how.
+    he truly saved us from the 
+    hell that is OpenGL.
+###########################
+"""
+
+"""
+when running this script, make sure these py files
+are present in the same pwd which this script is 
+located in!
+"""
 from back import *
 from card_example import *
 from plus_four import *
@@ -25,4 +47,29 @@ def draw(number, color = None):
 	elif number.upper() == 'B':
 		back()
 
+<<<<<<< HEAD
 draw(input("Number: "), input("Color: "))
+=======
+
+
+number = None
+while not number in valid_cards:
+	number = input("number: ").upper()
+
+if number in str({*range(10)}) or number == '+':
+	color = input("color: ")
+	drawCard(color, number)
+#color isnt important for some cards!
+elif number == 'P':
+	plus4()
+elif number == 'C':
+	wild()
+elif number == 'R':
+	color = input("color: ")
+	reverse(color)
+elif number == 'S':
+	color = input("color: ")
+	skip(color)
+elif number == 'B':
+	back()
+>>>>>>> d540d72 (added comments and attributions)
